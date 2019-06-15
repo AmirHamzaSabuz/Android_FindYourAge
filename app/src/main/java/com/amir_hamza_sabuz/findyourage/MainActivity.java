fa976a8d6c39808af2a7d33c2dda0df8426bd498
Amir Hamza <hamza_amir@yahoo.com>
2019-06-15 06:13:42 +0600

    Age calculation

---
+++ app/src/main/java/com/amir_hamza_sabuz/findyourage/MainActivity.java
@@ -0,0 +1,30 @@
+package com.amir_hamza_sabuz.findyourage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etBirthYear;
+    TextView tvAge;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etBirthYear= findViewById(R.id.etBirthYear);
+        tvAge = findViewById(R.id.tvAge);
+    }
+
+    public void buFindAge(View view) {
+        int birthYear = Integer.parseInt(etBirthYear.getText().toString());
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int age = year-birthYear;
+        tvAge.setText(String.valueOf(age));
+    }
+}
